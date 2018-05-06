4ba3c387e4771ac6dfe475418e3debd9ae7687de
Daschmied <DouglasSchmieder@yahoo.com>
2018-05-05 23:49:27 -0400

    added the project stuff thingy

---
+++ Google VR Project/Assets/Controller_Script.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_Script : MonoBehaviour {
+
+    private bool walking = false;
+    private Vector3 spawnPoint;
+
+
+	// Use this for initialization
+	void Start () {
+        spawnPoint = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(transform.position.y < -10f)
+        {
+            transform.position = spawnPoint;
+
+        }
+
+        Debug.Log("Here");
+
+		if(walking)
+        {
+            transform.position = transform.position + Camera.main.transform.forward * .5f * Time.deltaTime;
+
+        }
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(.5f, .5f, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if(hit.collider.name.Contains("plane"))
+            {
+                walking = false;
+            }
+            else
+            {
+                walking = true;
+            }
+        }
+	}
+}
---
+++ Google VR Project/Assets/Item_Selection.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Item_Selection : MonoBehaviour {
+
+    enum itemName {Banana, Book, Lego, SoccerBall, Hammer, WiiController, COUNT };
+
+    int select = -1;
+
+    public GameObject messageObject;
+
+    public string selectedItem = "Banana"; 
+    //public itemName selected = itemName.Banana;
+
+    public void Reset()
+    {
+        int sibIdx = transform.GetSiblingIndex();
+        int numSibs = transform.parent.childCount;
+        for (int i = 0; i < numSibs; i++)
+        {
+            GameObject sib = transform.parent.GetChild(i).gameObject;
+            //sib.transform.localPosition = startingPosition;
+            sib.SetActive(i == sibIdx);
+        }
+    }
+
+    public void objectSelected()
+    {
+        Debug.Log("Myself: " + gameObject.name);
+        Debug.Log("Selected: " + selectedItem);
+
+
+        if (gameObject.name == selectedItem)
+        {
+            Debug.Log("Choosing new item");
+            // Pick a random sibling, move them somewhere random, activate them,
+            // deactivate ourself.
+            int sibIdx = transform.GetSiblingIndex();
+            int numSibs = transform.parent.childCount;
+            sibIdx = (sibIdx + UnityEngine.Random.Range(1, numSibs)) % numSibs;
+            GameObject randomSib = transform.parent.GetChild(sibIdx).gameObject;
+            randomSib.GetComponent<Item_Selection>().selectedItem = randomSib.name;
+            Debug.Log("Random Sib: " + randomSib.name);
+            selectedItem = randomSib.name;
+
+            Text textObject = messageObject.GetComponent<Text>();
+
+            textObject.text = randomSib.name;
+            
+        }
+        // Move to random new location ±100º horzontal.
+        /*
+        Vector3 direction = Quaternion.Euler(
+            0,
+            UnityEngine.Random.Range(-90, 90),
+            0) * Vector3.forward;
+        */
+        // New location between 1.5m and 3.5m.
+        //float distance = 2 * UnityEngine.Random.value + 1.5f;
+        //Vector3 newPos = direction * distance;
+        // Limit vertical position to be fully in the room.
+        //newPos.y = Mathf.Clamp(newPos.y, -1.2f, 4f);
+        //randomSib.transform.localPosition = newPos;
+
+        //randomSib.SetActive(true);
+        //gameObject.SetActive(false);
+        //SetGazedAt(false);
+    }
+
+    /*
+    public void selectBanana()
+    {
+        Debug.Log("Click on Banana");
+        checkSelected(itemName.Banana);
+        
+    }
+
+    private void checkSelected(itemName selObject)
+    {
+        //throw new NotImplementedException();
+        if (selected == selObject)
+        {
+            Debug.Log("Found Correct Object");
+            resetItem();
+        }
+
+    }
+
+    public void resetItem()
+    {
+        selected = (itemName)(UnityEngine.Random.Range(0, Enum.GetNames(typeof(itemName)).Length));
+
+        Debug.Log("New Object: " + selected);
+    }
+    */
+
+    // Use this for initialization
+    void Start () {
+        //messageObject.text = "Thingy";
+        messageObject = GameObject.Find("ObjectToSelectText");
+        //messageObject = transform.parent.Find("ObjectMessage/ObjectToSelectText");  
+        Debug.Log(messageObject.name);
+        //Debug.Log("Object: " + selected);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
